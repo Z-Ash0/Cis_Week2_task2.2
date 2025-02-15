5940e1b4ef94384191461e89a90b999cc590067e
Z-Ash0 <zfire3005@gmail.com>
2025-02-15 02:02:44 +0200

    Setting-up our project environment

---
+++ android/app/src/main/kotlin/com/example/task2p2_w2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task2p2_w2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:task2p2_w2/feautures/homePage/presentation/view/home_page.dart';
+
+void main() {
+  runApp(const Task2P2W2());
+}
+
+class Task2P2W2 extends StatelessWidget {
+  const Task2P2W2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
